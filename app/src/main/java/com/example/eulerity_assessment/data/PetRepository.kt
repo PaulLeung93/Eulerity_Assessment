89dcfe2649e8053d4dfd711147bb3caa1ffded9d
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-09-10 00:29:29 -0400

    -Added URL in PetsRepository to properly fetch JSON -Customized theme colors

--- app/src/main/java/com/example/eulerity_assessment/data/PetRepository.kt
+++ app/src/main/java/com/example/eulerity_assessment/data/PetRepository.kt
@@ -1,9 +1,22 @@
-package com.example.eulerity_assessment.data
-
+import com.example.eulerity_assessment.data.PetsAPI
 import com.example.eulerity_assessment.domain.model.Pet
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class PetsRepository(private val apiService: PetsAPI) {
+class PetsRepository {
+
+    private val api: PetsAPI
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://eulerity-hackathon.appspot.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        api = retrofit.create(PetsAPI::class.java)
+    }
+
     suspend fun getPets(): List<Pet> {
-        return apiService.getPets()
+        return api.getPets()
     }
 }
