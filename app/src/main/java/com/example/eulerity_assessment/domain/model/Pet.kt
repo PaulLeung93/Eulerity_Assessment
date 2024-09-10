ef2c1f99373704774d8cb17b37a0207cdaea3124
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-09-09 23:42:42 -0400

    -Updated Pet data class to be serializable, allowing it to be passed using intents

--- app/src/main/java/com/example/eulerity_assessment/domain/model/Pet.kt
+++ app/src/main/java/com/example/eulerity_assessment/domain/model/Pet.kt
@@ -1,8 +1,10 @@
 package com.example.eulerity_assessment.domain.model
 
+import java.io.Serializable
+
 data class Pet(
     val title: String,
     val description: String,
     val url: String,
     val created: String
-)
+) : Serializable
