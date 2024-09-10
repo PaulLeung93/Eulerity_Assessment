cfee4f4acfa11554a76766a33fc4e453ba5bf0cc
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-09-10 05:14:08 -0400

    -Added toolbars to highlight the transition between XML and JetPack Compose between screens

--- app/src/main/java/com/example/eulerity_assessment/presentation/PetDetailsComposeActivity.kt
+++ app/src/main/java/com/example/eulerity_assessment/presentation/PetDetailsComposeActivity.kt
@@ -9,14 +9,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
+import com.example.eulerity_assessment.R
 import com.example.eulerity_assessment.domain.model.Pet
 import com.example.eulerity_assessment.ui.theme.Eulerity_AssessmentTheme
 
@@ -34,28 +41,41 @@ class PetDetailsComposeActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PetDetailsScreen(pet: Pet?) {
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Column(modifier = Modifier.padding(16.dp)) {
-            pet?.let {
-                AsyncImage(
-                    model = it.url,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp)
-                )
-                Text(
-                    text = it.title,
-                    style = MaterialTheme.typography.headlineMedium,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-                Text(
-                    text = it.description,
-                    style = MaterialTheme.typography.bodyLarge,
-                    modifier = Modifier.padding(top = 4.dp)
-                )
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(R.color.colorPrimary),
+                    titleContentColor = colorResource(R.color.colorOnPrimary),
+                ),
+                title = { Text("JetPack Compose") }
+            )
+        }
+    ) { innerPadding ->
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Column(modifier = Modifier.padding(innerPadding).padding(16.dp)) {
+                pet?.let {
+                    AsyncImage(
+                        model = it.url,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                    )
+                    Text(
+                        text = it.title,
+                        style = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier.padding(top = 8.dp)
+                    )
+                    Text(
+                        text = it.description,
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(top = 4.dp)
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/example/eulerity_assessment/presentation/PetsActivity.kt
+++ app/src/main/java/com/example/eulerity_assessment/presentation/PetsActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
@@ -21,6 +22,10 @@ class PetsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pets_list)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = getString(R.string.activity_screen)
 
         viewModel = ViewModelProvider(this).get(PetsViewModel::class.java)
         petsAdapter = PetsAdapter { pet -> onPetClicked(pet) }
